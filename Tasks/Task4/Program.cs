3477190fb2696c5315eab5d3c36315fceaaa7b67
«Svetlana <passiong@mail.ru>
2022-02-06 00:51:23 +0500

    Task4

---
+++ Tasks/Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти максимальное из трех чисел
+
+int numberA = new Random().Next(1, 100);
+Console.WriteLine($"Первое число: {numberA}");
+
+int numberB = new Random().Next(1, 100);
+Console.WriteLine($"Второе число: {numberB}");
+
+int numberC = new Random().Next(1, 100);
+Console.WriteLine($"Третье число: {numberC}");
+
+int max = numberA;
+if (numberB > max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.WriteLine($"Максимальное число: {max}");
