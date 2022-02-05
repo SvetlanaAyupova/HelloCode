c2702607bc5952d770a17e1c933d23d3d4b1ed1c
«Svetlana <passiong@mail.ru>
2022-02-05 17:26:33 +0500

    Task0

---
+++ Tasks/Task0/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывести квадрат числа
+
+Console.Write("Введите число ->");
+
+String numberStr = Console.ReadLine();
+
+int number = int.Parse(numberStr);
+
+Console.WriteLine($"Квадрат числа:{number*number}");
