f3c22db2b82087dcc7eadf6f7fbedc89df76aea0
«Svetlana <passiong@mail.ru>
2022-02-06 00:27:24 +0500

    Task2

---
+++ Tasks/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Даны два числа. Показать большее и меньшее число
+
+int numberA = new Random().Next(1, 100);
+Console.WriteLine($"Первое число: {numberA}");
+int numberB = new Random().Next(1, 100);
+Console.WriteLine($"Второе число: {numberB}");
+if (numberA > numberB)
+{
+    Console.WriteLine($"Большее число: {numberA}");
+    Console.WriteLine($"Меньшее число: {numberB}");
+}
+else
+{
+    Console.WriteLine($"Большее число: {numberB}");
+    Console.WriteLine($"Меньшее число: {numberA}");
+}
+if (numberA == numberB)
+{
+    Console.WriteLine("Числа равны");
+}
