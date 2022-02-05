0c6ba743108b1f33d81e1f34b3ab04c7abbe0420
«Svetlana <passiong@mail.ru>
2022-02-05 23:39:53 +0500

    Task7

---
+++ Tasks/Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать числа от -N до N
+Console.Write("Введите число N: ");
+string numN = Console.ReadLine();
+int number = int.Parse(numN);
+int min = -number;
+while (min <= number)
+{
+    Console.WriteLine(min);
+    min++;
+}
