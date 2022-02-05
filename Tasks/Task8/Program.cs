20bd3372143692f29594a14059605c2394431446
«Svetlana <passiong@mail.ru>
2022-02-06 02:01:39 +0500

    Task8

---
+++ Tasks/Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать четные числа от 1 до N
+
+Console.WriteLine("Введите число N: ");
+string? numN = Console.ReadLine();
+int number = int.Parse(numN);
+int min = 2;
+while (min <= number)
+{
+    if (min % 2 == 0) Console.WriteLine($"Чётные числа: {min}");
+    min++;
+}
