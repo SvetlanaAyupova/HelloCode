2463bb050ecb24162b4227daa8d83e20319afa7e
«Svetlana <passiong@mail.ru>
2022-02-05 21:56:34 +0500

    Task5

---
+++ Tasks/Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+
+Console.WriteLine("y = 7*x*x + 3*x + 6");
+
+Console.Write("Введите значение x: ");
+
+String? numberStr = Console.ReadLine();
+
+int x = int.Parse(numberStr);
+
+Console.WriteLine($"y = { 7 * x * x + 3 * x + 6 } ");
