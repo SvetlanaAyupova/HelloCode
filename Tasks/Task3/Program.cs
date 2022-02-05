ca38fe2189c446ce84d4612b7af0b4211fe855eb
«Svetlana <passiong@mail.ru>
2022-02-05 20:08:50 +0500

    Task3

---
+++ Tasks/Task3/Program.cs
@@ -0,0 +1,47 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("Введите номер дня недели ->");
+
+String? numberStr = Console.ReadLine();
+
+int number = int.Parse(numberStr);
+
+if (number == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+
+if (number == 2)
+{
+    Console.WriteLine("Вторник");
+}
+
+if (number == 3)
+{
+    Console.WriteLine("Среда");
+}
+
+if (number == 4)
+{
+    Console.WriteLine("Четверг");
+}
+
+if (number == 5)
+{
+    Console.WriteLine("Пятница");
+}
+
+if (number == 6)
+{
+    Console.WriteLine("Суббота");
+}
+
+if (number == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+if (number > 7)
+{
+    Console.WriteLine("Такого номера дня недели нет");
+}
