8eefd783a7dc28d403c342429dde998d2ae2e1a6
«Svetlana <passiong@mail.ru>
2022-02-06 22:42:31 +0500

    Task15

---
+++ Tasks/Task15/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+bool CheckIfModed(int number)
+{
+    return number % 7 ==0 && number % 23 ==0;
+}
+
+
+Console.Write("Введите число -> ");
+
+String? numberStr = Console.ReadLine();
+
+int number = int.Parse(numberStr);
+
+if (CheckIfModed(number))
+    Console.WriteLine($"Число {number} кратно 7 и 23");
+else
+    Console.WriteLine($"Число {number} не кратно 7 и 23");
