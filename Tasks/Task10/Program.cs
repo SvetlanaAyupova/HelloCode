78f05a412b39bdea1dcda0f95c8a1aa8e6782b23
«Svetlana <passiong@mail.ru>
2022-02-06 23:36:06 +0500

    Task10

---
+++ Tasks/Task10/Program.cs
@@ -0,0 +1,25 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int GetSecondDigit(int number)
+{
+    int newNumber = number / 10;
+    return newNumber % 10;
+}
+
+int GetIntegerFromConsole(int min, int max)
+{
+    int result = 0;
+    bool isError = true;
+    while (isError)
+    {
+        Console.Write($"Введите целое число от {min} по {max}: ");
+        string input = Console.ReadLine();
+        result = int.Parse(input);
+        isError = result<min || result>max;
+    }
+    return result;
+}
+
+int number = GetIntegerFromConsole(100,999);
+int secondDigit = GetSecondDigit(number);
+Console.WriteLine(secondDigit);
