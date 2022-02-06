cb3de8addd9a4139d816cab59aee2a8a1d5e0375
«Svetlana <passiong@mail.ru>
2022-02-06 19:54:16 +0500

    Task9

---
+++ Tasks/Task9/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+int GetLastDigit(int number)
+{
+    return number % 10;
+}
+
+int GetIntegerFromConsole(int min, int max)
+{
+    int result = 0;
+    bool isError = true;
+    while (isError)
+    {
+        Console.Write($"Введите целое число от {min} по {max}: ");
+        string input = Console.ReadLine();
+        result = int.Parse(input);
+        isError = result<min || result>max;
+    }
+    return result;
+}
+
+int number = GetIntegerFromConsole(100,999);
+int lastDigit = GetLastDigit(number);
+Console.WriteLine(lastDigit);
