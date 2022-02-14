618ed1852b428004ed90263bea64d75a9b65df13
«Svetlana <passiong@mail.ru>
2022-02-14 20:24:27 +0500

    Task18

---
+++ Tasks/Task18/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+void CheckTruth(bool X, bool Y)
+{
+    Console.WriteLine($"!({X} || {Y}) = {!(X || Y)}, !{X} && !{Y} = {!X && !Y}");
+}
+
+CheckTruth(false, false);
+CheckTruth(false, true);
+CheckTruth(true, false);
+CheckTruth(true, true);
