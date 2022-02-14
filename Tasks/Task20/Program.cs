cb90e697edc031121c104d7432c05301a6a65837
«Svetlana <passiong@mail.ru>
2022-02-14 20:59:38 +0500

    Task20

---
+++ Tasks/Task20/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+int InPut(string msg)
+{
+    Console.Write(msg);
+    string? num1 = Console.ReadLine();
+    return int.Parse(num1);
+}
+
+
+
+void Execute(int n)
+{
+    if (n == 1)
+        Console.WriteLine("Координата x > 0 и координата y > 0");
+    else if (n == 2)
+        Console.WriteLine("Координата x < 0 и координата y > 0");
+    else if (n == 3)
+        Console.WriteLine("Координата x < 0 и координата y < 0");
+    else if (n == 4)
+        Console.WriteLine("Координата x > 0 и координата y < 0");
+}
+
+int n = InPut("Введите номер четверти координатной плоскости: ");
+
+
+Execute(n);
