0af6feef438fc005fe1ec7ef9bc2c3c8bd465000
«Svetlana <passiong@mail.ru>
2022-02-16 23:46:24 +0500

    Task28

---
+++ Tasks/Task28/Program.cs
@@ -0,0 +1,20 @@
+﻿// Подсчитать сумму цифр в числе
+
+int InPut(string msg)
+{
+    Console.Write(msg);
+    string? num1 = Console.ReadLine();
+    int A = int.Parse(num1);
+    return A;
+}
+
+int number = InPut("Введите число: ");
+
+int sumOfDigit = 0;
+while (number > 0)
+{
+    sumOfDigit = sumOfDigit + number % 10;
+    number = number /10 ;
+}
+
+Console.WriteLine($"Сумма цифр в числе: {sumOfDigit}");
