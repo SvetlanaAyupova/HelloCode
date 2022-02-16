6a64b4b8e1a9b0fae68ea4f2a226b06db8d8575d
«Svetlana <passiong@mail.ru>
2022-02-16 20:19:31 +0500

    Task27

---
+++ Tasks/Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Определить количество цифр в числе
+
+int InPut(string msg)
+{
+    Console.Write(msg);
+    string? num1 = Console.ReadLine();
+    int A = int.Parse(num1);
+    return A;
+}
+
+int number = InPut("Введите число: ");
+
+int countOfDigit = 0;
+while (number != 0)
+{
+    number = number/10;
+    countOfDigit++;
+} 
+    
+
+Console.WriteLine(countOfDigit);
