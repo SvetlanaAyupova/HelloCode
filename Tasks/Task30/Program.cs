4f7ff6273fba78234f8f75c3e95545c2984a2d1e
«Svetlana <passiong@mail.ru>
2022-02-17 21:22:31 +0500

    Task30

---
+++ Tasks/Task30/Program.cs
@@ -0,0 +1,24 @@
+﻿// Показать кубы чисел, заканчивающихся на чётную цифру.
+// Если число заканчивается на чётную цифру, значит всё число является четным.
+
+int InPut(string msg)
+{
+    Console.Write(msg);
+    string? num1 = Console.ReadLine();
+    int A = int.Parse(num1);
+    return A;
+}
+
+int n = InPut("Введите число: ");
+
+for (int i = 1; i <= n; i++)
+{
+    if (i % 2 == 0) Console.WriteLine($"Куб числа {i} = {i * i * i}");
+}
+
+
+
+
+
+
+
