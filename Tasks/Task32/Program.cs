57d56c57f6263ded6c5664d4af66ea95e2befad6
«Svetlana <passiong@mail.ru>
2022-02-17 21:31:44 +0500

    Task32

---
+++ Tasks/Task32/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задать массив из N элементов, заполненных нулями и единицами вывести их на экран.
+
+int GetIntFromConsole()
+{
+    Console.Write("Введите число от 1 до 20: ");
+    string? num1 = Console.ReadLine();
+    int A = int.Parse(num1);
+    return A;
+}
+
+int[] ArrayInit(int num)
+{
+    int[] array = new int[num];
+    Random randomNum = new Random();
+    for (int i = 0; i < num; i++)
+    {
+        array[i] = randomNum.Next(0, 2);
+    }
+    return array;
+}
+
+void ArrayPrint(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int getInt = GetIntFromConsole();
+int[] array = ArrayInit(getInt);
+
+ArrayPrint(array);
