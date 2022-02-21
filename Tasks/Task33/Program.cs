509741be3500f255ff8714f49222272b45049e6a
«Svetlana <passiong@mail.ru>
2022-02-21 13:00:55 +0500

    Task33

---
+++ Tasks/Task33/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9].
+// Найти сумму положительных/отрицательных элементов массива.
+
+int sumPol = 0;
+int sumOtr = 0;
+int[] array = new int[12];
+Console.WriteLine("Создали массив");
+
+Random randomNum = new Random();
+for (int i = 0; i < 12; i++)
+{
+    array[i] = randomNum.Next(-9, 10);
+    Console.Write($"{array[i]} ");
+    if (array[i] > 0)
+        sumPol = sumPol + array[i];
+    else
+        sumOtr = sumOtr + array[i];
+}
+
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных чисел = {sumPol}");
+Console.WriteLine($"Сумма отрицательных чисел = {sumOtr}");
