fead5031a8e543f5768717d76ec0cfeb21fe1b0f
«Svetlana <passiong@mail.ru>
2022-02-21 21:50:36 +0500

    Task40

---
+++ Tasks/Task40/Program.cs
@@ -0,0 +1,33 @@
+﻿// В указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом.
+
+double[] arr = new double[10];
+
+void FullArray()
+{
+    Random number = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = number.Next(-20, 20) + number.NextDouble();
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+FullArray();
+Console.WriteLine();
+
+
+double max = arr[0];
+double min = arr[0];
+
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > max) max = arr[i];
+    if (arr[i] < min) min = arr[i];
+}
+
+double difference = max - min;
+
+Console.WriteLine($"Максимальный элемент массива: {max}");
+Console.WriteLine($"Минимальный элемент массива: {min}");
+Console.WriteLine($"Разница между максимальным и минимальным элементом: {difference}");
