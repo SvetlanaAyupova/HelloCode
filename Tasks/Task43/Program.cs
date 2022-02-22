dad6f11876df1f41706812b8004dec5cfe1c3aa6
«Svetlana <passiong@mail.ru>
2022-02-22 12:38:38 +0500

    Task43

---
+++ Tasks/Task43/Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу преобразования десятичного числа в двоичное.
+
+int InPut()
+{
+    Console.Write("Введите десятичное число: ");
+    string? input = Console.ReadLine();
+    int.TryParse(input, out int number);
+    return number;
+}
+
+int number = InPut();
+int numberOst = 0;
+int result = 0;
+int k = 1; // Разряд числа.
+
+
+while (number != 0)
+{
+    numberOst = number % 2;
+    number = number / 2;
+    result += numberOst * k;
+    k = k * 10;
+}
+
+Console.WriteLine($"Двоичное число: {result}");
+
+
+
+
+
