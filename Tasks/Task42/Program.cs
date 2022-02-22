7f0c78b72ad493a3fe3dff55fda2ceb7d45345f3
«Svetlana <passiong@mail.ru>
2022-02-22 17:30:06 +0500

    Task42

---
+++ Tasks/Task42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Определить сколько чисел больше 0 введено с клавиатуры.
+
+
+void Main()
+{
+    Console.Write("Введите количество числе N: ");
+    int n = int.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Введите {n} чисел: ");
+    int[] arr = new int[n];
+    for (int i = 0; i < arr.Length; i++)
+        arr[i] = int.Parse(Console.ReadLine());
+    int count = 0;
+    for(int i=0;i<arr.Length;i++)
+        if(arr[i] > 0)
+            count++;
+    Console.WriteLine($"Количество чисел больше 0 = {count}.");
+}
+
+Main();
