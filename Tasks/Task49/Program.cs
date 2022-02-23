318896ca91031ab0afb2a13e780eb6bac7c41ed1
«Svetlana <passiong@mail.ru>
2022-02-23 23:51:14 +0500

    Task49

---
+++ Tasks/Task49/Program.cs
@@ -0,0 +1,30 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами.
+
+double[,] arr = new double[6, 5];
+Random rand = new Random();
+
+void FillArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = Math.Round(rand.NextDouble()*100, 2);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray();
+PrintArray();
