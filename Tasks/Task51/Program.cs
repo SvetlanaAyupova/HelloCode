7627a8abb32ad27486892cce5ffa88f26f5942b7
«Svetlana <passiong@mail.ru>
2022-02-24 00:24:00 +0500

    Task51

---
+++ Tasks/Task51/Program.cs
@@ -0,0 +1,46 @@
+﻿// 51. Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+int[,] arr = new int[2, 2];
+Random rand = new Random();
+
+void FillArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = rand.Next(1, 9);
+        }
+    }
+}
+
+int [,] MethodSum()
+{
+    int[,] newArr = new int[arr.GetLength(0), arr.GetLength(1)];
+    for (int i = 0; i < newArr.GetLength(0); i++)
+    {
+        for (int j = 0; j < newArr.GetLength(1); j++)
+        {
+            newArr[i,j] = i+j;
+        }
+    }
+    return newArr;
+}
+
+void PrintArray(int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillArray();
+PrintArray(arr);
+Console.WriteLine();
+int[,] newArr = MethodSum();
+PrintArray(newArr);
