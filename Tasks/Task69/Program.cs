d8cc8094290afca543ecb748667a336da7d5d151
«Svetlana <passiong@mail.ru>
2022-03-04 15:48:53 +0500

    Task69

---
+++ Tasks/Task69/Program.cs
@@ -0,0 +1,40 @@
+﻿// Найти сумму элементов от M до N, N и M заданы.
+
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    bool checkStatus = true;
+    int numberInt = 0;
+    while (checkStatus)
+    {
+        string numberStr = Console.ReadLine();
+        if (int.TryParse(numberStr, out numberInt))
+        {
+            checkStatus = false;
+            break;
+        }
+        else
+            Console.WriteLine("Ввели не число");
+    }
+    return numberInt;
+}
+
+int Sum(int m, int n)
+{
+    int sum = 0;
+    if (m < n)
+        for (int i = m; i <= n; i++)
+        {
+            sum += i;
+        }
+    else
+        for (int i = m; i >= n; i--)
+        {
+            sum += i;
+        }
+    return sum;
+}
+
+int m = GetNumber("Введите число m: ");
+int n = GetNumber("Введите число n: ");
+Console.WriteLine($"Сумма натуральных чисел в промежутке от {m} до {n} = {Sum(m, n)}");
